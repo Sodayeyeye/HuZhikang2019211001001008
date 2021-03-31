08a82c926a83be2f360a65e034c8292496a2b828
2019211001001008HuZhiKang <1911316449@qq.om>
2021-03-31 15:46:30 +0800

    HW-Week 2 Commit

--- src/main/java/com/HuZhiKang/week2/demo/HelloWorldServlet.java
+++ src/main/java/com/HuZhiKang/week2/demo/HelloWorldServlet.java
@@ -6,11 +6,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class HelloWorldServlet extends HttpServlet {
 
+
+
+public class HelloWorldServlet extends HttpServlet {
+    String name="HuZhiKang";
+    String ID="2019211001001008";
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
         writer.println("Hello Client !!!");
+        writer.println("Name: "+name+"<br/>");
+        writer.println("ID: "+ID+"<br/>");
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response){
 
